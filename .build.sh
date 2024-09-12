30e676012403b783e981bd8a2d8f5f33dee8b199
Brett <brett.whynot@gmail.com>
2024-09-12 10:56:09 +1000

    Update build

--- .build.sh
+++ .build.sh
@@ -2,5 +2,9 @@
 cd "${0%/*}"
 OUTPUT="${1:-TA-hetrix.spl}"
 pip install --upgrade --no-dependencies -t lib -r lib/requirements.txt
-rm -rf lib/*.dist-info
-tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite ../TA-hetrix 
+rm -rf lib/splunklib/__pycache__
+rm -rf lib/splunklib/searchcommands/__pycache__
+rm -rf lib/splunklib/modularinput/__pycache__
+rm -rf lib/*/__pycache__
+cd ..
+tar -cpzf $OUTPUT --exclude=.* --exclude=package.json --overwrite TA-hetrix 
