fa86ffb1fd0b317aca822c72c0c6f6fc2badbcd3
Brett <brett.whynot@gmail.com>
2024-11-19 09:57:26 +1000

    Update build to remove splunklib before install

--- .build.sh
+++ .build.sh
@@ -4,6 +4,7 @@ OUTPUT="${1:-TA-hetrix.spl}"
 chmod -R u=rwX,go= *
 chmod -R u-x+X *
 chmod -R u=rwx,go= bin/*
+rm -rf lib/splunk*
 python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
 rm -rf lib/*/__pycache__
 rm -rf lib/*/*/__pycache__
